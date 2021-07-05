1084221351c5a2d569f4dc056c050818cc099bcd
Felipe Higa <felipe_massao@hotmail.com>
2021-07-04 23:39:41 -0300

    Model update

--- Model.py
+++ Model.py
@@ -6,4 +6,66 @@ from sklearn_pandas import DataFrameMapper
 # %%
 data=pd.read_csv('Data/mpg_data_example.csv')
 # %%
-dsadasfasdf
+data.head()
+
+# %%
+data.isnull().sum()
+
+# %%
+
+mapper = DataFrameMapper([
+                        (['cylinders','displacement','weight','acceleration','model year'], preprocessing.StandardScaler()),
+                        (['horsepower'],preprocessing.Imputer()),
+                        (['origin'], preprocessing.OneHotEncoder())
+                        ])
+
+# %%
+pipeline_obj = pipeline.Pipeline([
+    ('mapper',mapper),
+    ("model", ensemble.RandomForestRegressor())
+])
+
+# %%
+data.columns
+
+# %%
+X=['cylinders', 'displacement', 'horsepower', 'weight',
+       'acceleration', 'model year', 'origin']
+Y=['mpg']
+
+# %%
+pipeline_obj.fit(data[X],data[Y])
+
+# %%
+pipeline_obj.predict(data[X])
+
+# %%
+from sklearn.externals import joblib
+
+# %%
+joblib.dump(pipeline_obj,'RFModelforMPG.pkl')
+
+# %%
+modelReload=joblib.load('RFModelforMPG.pkl')
+
+# %%
+modelReload.predict(data[X])
+
+# %%
+temp={}
+temp['cylinders']=1
+temp['displacement']=2
+temp['horsepower']=3
+temp['weight']=4
+temp['acceleration']=5
+temp['model year']=6
+temp['origin']=1
+
+# %%
+testDtaa=pd.DataFrame({'x':temp}).transpose()
+
+# %%
+testDtaa
+
+# %%
+modelReload.predict(testDtaa)[0]
